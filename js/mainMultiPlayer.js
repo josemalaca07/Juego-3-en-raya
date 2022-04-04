3b9a2e857b15af1afca0756449220787137c749a
josemalaca07 <98763093+josemalaca07@users.noreply.github.com>
2022-04-04 22:57:35 +0200

    v.1.1
    -Arreglado el fallo en los enlaces multiplayer y singleplayer

--- js/mainMultiPlayer.js
+++ js/mainMultiPlayer.js
@@ -1,7 +1,10 @@
-//JUEGO
+var estadoPartida="NoTerminada"
+var turno=0;
+
 $(document).ready(function(){
     reiniciarJuego();
 });
+
 
 function reiniciarJuego(){
     for(var x=1;x<=9;x++){
@@ -15,9 +18,18 @@ $("#resetButon").click(function(){
 
 $("#celda1").click(function(){
     if(document.getElementById("celda1").className != "celda marked"){
-        $("#img1").show();
-        $(this).addClass('marked');
-        juegomaquina();
+        if(turno%2==0){ //EQUIS
+            document.getElementById("celda1").innerHTML='<img src="./assets/equis.png" id="img1" class="img-fluid">';
+            $("#img1").show();
+            $(this).addClass('marked');
+            turno++;
+        }
+        else { //CIRCULO
+            document.getElementById("celda1").innerHTML='<img src="./assets/circulo.png" id="img1" class="img-fluid">';
+            $("#img1").show();
+            $(this).addClass('marked');
+            turno++
+        }
     }else{
         alert("Esta celda ya esta marcada");
     }
@@ -25,112 +37,152 @@ $("#celda1").click(function(){
 
 $("#celda2").click(function(){
     if(document.getElementById("celda2").className != "celda marked"){
-    $("#img2").show();
-    $(this).addClass('marked');
-    juegomaquina();
-}else{
-    alert("Esta celda ya esta marcada");
-}
+        if(turno%2==0){ //EQUIS
+            document.getElementById("celda2").innerHTML='<img src="./assets/equis.png" id="img2" class="img-fluid">';
+            $("#img2").show();
+            $(this).addClass('marked');
+            turno++;
+        }
+        else{ //CIRCULO
+            document.getElementById("celda2").innerHTML='<img src="./assets/circulo.png" id="img2" class="img-fluid">';
+            $("#img2").show();
+            $(this).addClass('marked');
+            turno++
+        }
+    }else{
+        alert("Esta celda ya esta marcada");
+    }
 });
 
 $("#celda3").click(function(){
     if(document.getElementById("celda3").className != "celda marked"){
-    $("#img3").show();
-    $(this).addClass('marked');
-    juegomaquina();
-}else{
-    alert("Esta celda ya esta marcada");
-}
+        if(turno%2==0){ //EQUIS
+            document.getElementById("celda3").innerHTML='<img src="./assets/equis.png" id="img3" class="img-fluid">';
+            $("#img3").show();
+            $(this).addClass('marked');
+            turno++;
+        }
+        else{ //CIRCULO
+            document.getElementById("celda3").innerHTML='<img src="./assets/circulo.png" id="img3" class="img-fluid">';
+            $("#img3").show();
+            $(this).addClass('marked');
+            turno++
+        }
+    }else{
+        alert("Esta celda ya esta marcada");
+    }
 });
 
 $("#celda4").click(function(){
     if(document.getElementById("celda4").className != "celda marked"){
-    $("#img4").show();
-    $(this).addClass('marked');
-    juegomaquina();
-}else{
-    alert("Esta celda ya esta marcada");
-}
+        if(turno%2==0){ //EQUIS
+            document.getElementById("celda4").innerHTML='<img src="./assets/equis.png" id="img4" class="img-fluid">';
+            $("#img4").show();
+            $(this).addClass('marked');
+            turno++;
+        }
+        else{ //CIRCULO
+            document.getElementById("celda4").innerHTML='<img src="./assets/circulo.png" id="img4" class="img-fluid">';
+            $("#img4").show();
+            $(this).addClass('marked');
+            turno++
+        }
+    }else{
+        alert("Esta celda ya esta marcada");
+    }
 });
 
 $("#celda5").click(function(){
     if(document.getElementById("celda5").className != "celda marked"){
-    $("#img5").show();
-    $(this).addClass('marked');
-    juegomaquina();
-}else{
-    alert("Esta celda ya esta marcada");
-}
+        if(turno%2==0){ //EQUIS
+            document.getElementById("celda5").innerHTML='<img src="./assets/equis.png" id="img5" class="img-fluid">';
+            $("#img5").show();
+            $(this).addClass('marked');
+            turno++;
+        }
+        else{ //CIRCULO
+            document.getElementById("celda5").innerHTML='<img src="./assets/circulo.png" id="img5" class="img-fluid">';
+            $("#img5").show();
+            $(this).addClass('marked');
+            turno++
+        }
+    }else{
+        alert("Esta celda ya esta marcada");
+    }
 });
 
 $("#celda6").click(function(){
     if(document.getElementById("celda6").className != "celda marked"){
-    $("#img6").show();
-    $(this).addClass('marked');
-    juegomaquina();
-}else{
-    alert("Esta celda ya esta marcada");
-}
+        if(turno%2==0){ //EQUIS
+            document.getElementById("celda6").innerHTML='<img src="./assets/equis.png" id="img6" class="img-fluid">';
+            $("#img6").show();
+            $(this).addClass('marked');
+            turno++;
+        }
+        else{ //CIRCULO
+            document.getElementById("celda6").innerHTML='<img src="./assets/circulo.png" id="img6" class="img-fluid">';
+            $("#img6").show();
+            $(this).addClass('marked');
+            turno++
+        }
+    }else{
+        alert("Esta celda ya esta marcada");
+    }
 });
 
 $("#celda7").click(function(){
     if(document.getElementById("celda7").className != "celda marked"){
-    $("#img7").show();
-    $(this).addClass('marked');
-    juegomaquina();
-}else{
-    alert("Esta celda ya esta marcada");
-}
+        if(turno%2==0){ //EQUIS
+            document.getElementById("celda7").innerHTML='<img src="./assets/equis.png" id="img7" class="img-fluid">';
+            $("#img7").show();
+            $(this).addClass('marked');
+            turno++;
+        }
+        else{ //CIRCULO
+            document.getElementById("celda7").innerHTML='<img src="./assets/circulo.png" id="img7" class="img-fluid">';
+            $("#img7").show();
+            $(this).addClass('marked');
+            turno++
+        }
+    }else{
+        alert("Esta celda ya esta marcada");
+    }
 });
 
 $("#celda8").click(function(){
     if(document.getElementById("celda8").className != "celda marked"){
-    $("#img8").show();
-    $(this).addClass('marked');
-    juegomaquina();
-}else{
-    alert("Esta celda ya esta marcada");
-}
+        if(turno%2==0){ //EQUIS
+            document.getElementById("celda8").innerHTML='<img src="./assets/equis.png" id="img8" class="img-fluid">';
+            $("#img8").show();
+            $(this).addClass('marked');
+            turno++;
+        }
+        else{ //CIRCULO
+            document.getElementById("celda8").innerHTML='<img src="./assets/circulo.png" id="img8" class="img-fluid">';
+            $("#img8").show();
+            $(this).addClass('marked');
+            turno++
+        }
+    }else{
+        alert("Esta celda ya esta marcada");
+    }
 });
 
 $("#celda9").click(function(){
     if(document.getElementById("celda9").className != "celda marked"){
-    $(this).addClass('marked');
-    $("#img9").show();
-    juegomaquina();
-}else{
-    alert("Esta celda ya esta marcada");
-}
-});
-
-function getRandomChoice() {
-    return Math.floor(Math.random() * (10 - 0)) + 0;
-}
-
-function juegomaquina(){
-    try{
-        contador=0;
-        while(contador==0){
-        randomChoice = getRandomChoice();
-        if(document.getElementById("celda"+randomChoice).className == "celda"){
-            $("#celda"+randomChoice).addClass('marked');
-            mostrarX(randomChoice);
-            contador++;
-        }else{
-            console.log("marked");
+        if(turno%2==0){ //EQUIS
+            document.getElementById("celda9").innerHTML='<img src="./assets/equis.png" id="img9" class="img-fluid">';
+            $("#img2").show();
+            $(this).addClass('marked');
+            turno++;
         }
-    } 
-    }catch{
-        alert("Ha ocurrido un error refresque la pagia");
-        location.reload();
-    }  
-}
-
-function mostrarX(randomChoice){
-    document.getElementById("celda"+randomChoice).innerHTML="<img src='./assets/equis.png' id='img"+randomChoice+"' class='img-fluid'>";
-        $("#img"+randomChoice).show()
-        $("#img"+randomChoice).animate({
-            height: '5em',
-            width: '5em'
-        });
-}
+        else{ //CIRCULO
+            document.getElementById("celda9").innerHTML='<img src="./assets/circulo.png" id="img9" class="img-fluid">';
+            $("#img9").show();
+            $(this).addClass('marked');
+            turno++
+        }
+    }else{
+        alert("Esta celda ya esta marcada");
+    }
+});
